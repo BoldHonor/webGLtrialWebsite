9d34b86f591c98d21c5dc2708046a6907cfb02e5
Prakhar_Srivastava <57177086+BoldHonor@users.noreply.github.com>
2020-01-28 17:51:41 +0530

    Update canvas1.js

--- canvas1.js
+++ canvas1.js
@@ -25,7 +25,7 @@ var fragmentShaderText =
 ].join('\n');
 
 var mesh = new obj_loader.Mesh( "cube.obj" );
-console.log(mesh.vertices);
+
 
 var InitDemo = function () {
 	console.log('This is working');
